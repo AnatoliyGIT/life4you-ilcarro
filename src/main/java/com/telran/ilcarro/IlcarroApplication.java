47b83b6169e45a4956229224e23e81565a0eb207
AnatoliyGIT <kovrigin.anatoliy@gmail.com>
2020-04-29 17:56:25 +0300

    deleted @EnebleZuulProxy

--- src/main/java/com/telran/ilcarro/IlcarroApplication.java
+++ src/main/java/com/telran/ilcarro/IlcarroApplication.java
@@ -7,7 +7,7 @@ import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-@EnableZuulProxy
+//@EnableZuulProxy
 public class IlcarroApplication {
 
     public static void main(String[] args) {
